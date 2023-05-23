c5a128ebb7f6cf455c411d016531f19dcbebc302
plpratyusha <plpratyusha@gmail.com>
2023-05-23 15:48:23 -0700

    Modified to use props to pass in parameter values for new ExpenseItems instead of hardcoded values

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,18 +3,20 @@ import './ExpenseItem.css'; // import css files into this file
 // React component code is written basically as a function that returns html code
 // special bc it returns jsx code
 
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 3, 3);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
+function ExpenseItem(props) { // instead of parameters, props (key value pairs)
+  // const expenseDate = new Date(2023, 3, 3); // some javascript code; html + css are below in the function
+  // const expenseTitle = 'Car Insurance';
+  // const expenseAmount = 294.67;
+
+  // don't need these hardcoded values; using expenses array in App.js now
 
   // can only return one root element per return statement- use () and wrapping divs to do this
   return (
-    <div className="expense-item"> {/* not class bc not html/js, it's jsx */}
-      <div>{expenseDate.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+    <div className="expense-item"> {/* not class, it's className bc not html/js, it's jsx */}
+      <div>{props.date?.toISOString()}</div> 
+      <div className="expense-item__description"> 
+        <h2>{props.title}</h2> {/* injected constants in html code */}
+        <div className="expense-item__price">${props.amount}</div> {/*output $ hard coded*/}
       </div>
     </div>
   );
